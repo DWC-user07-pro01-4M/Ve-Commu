ad2c310ff52835d4861acbd5fa92e86d5fa1e348
DWC-user07-pro01-4M <124852092+DWC-user07-pro01-4M@users.noreply.github.com>
2023-05-06 07:18:34 +0000

    Add publicコントローラhomes追加

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,9 @@
 Rails.application.routes.draw do
 
+  namespace :public do
+    get 'homes/top'
+    get 'homes/about'
+  end
   namespace :admin do
     get 'comments/index'
     get 'comments/show'
