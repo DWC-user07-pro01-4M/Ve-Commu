81975f8ae0fd898dd4452648b8daba0f9198ef80
DWC-user07-pro01-4M <124852092+DWC-user07-pro01-4M@users.noreply.github.com>
2023-05-06 02:36:00 +0000

    Add end_userにintroductionカラムを追加

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2023_05_05_121856) do
+ActiveRecord::Schema.define(version: 2023_05_06_022026) do
 
   create_table "admins", force: :cascade do |t|
     t.string "email", default: "", null: false
@@ -34,6 +34,7 @@ ActiveRecord::Schema.define(version: 2023_05_05_121856) do
     t.datetime "updated_at", precision: 6, null: false
     t.string "nickname", null: false
     t.boolean "is_deleted", default: false
+    t.text "introduction"
     t.index ["email"], name: "index_end_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_end_users_on_reset_password_token", unique: true
   end
