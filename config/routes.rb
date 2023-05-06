8d37550b3c3396e15e380c612cf427ffec3327b6
DWC-user07-pro01-4M <124852092+DWC-user07-pro01-4M@users.noreply.github.com>
2023-05-06 07:41:38 +0000

    Add publicコントローラend_users追加、ルーティングを一部修正

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,41 @@
 Rails.application.routes.draw do
 
+  # 管理者
+  devise_for :admin, skip: [:registrations, :passwords] ,controllers: {
+    sessions: "admin/sessions"
+  }
+  namespace :admin do
+    #homes
+    root to: "homes#top"
+    #end_users
+    resources :end_users, only: [:index, :show, :edit, :update]
+    #tags
+    resources :tags, only: [:index, :create, :edit, :update, :destroy]
+    #comments
+    resources :comments, only: [:index, :show, :edit, :update, :destroy]
+    #order_items
+    resources :order_items, only: [:update]
+  end
+
+  # 会員
+  devise_for :end_users,skip: [:passwords], controllers: {
+    registrations: "public/registrations",
+    sessions: 'public/sessions'
+  }
+
+    scope module: :public do
+    root to: "homes#top"
+    get 'about' => 'homes#about'
+
+    resources :items, only: [:index, :show]
+    resources :customers, only: [:show, :edit, :update] do
+      collection do
+        get 'unsubscribe'
+        patch 'withdrawal'
+      end
+    end
+
+
   namespace :public do
     get 'bookmarks/index'
     get 'bookmarks/show'
@@ -10,37 +46,7 @@ Rails.application.routes.draw do
     get 'posts/show'
     get 'posts/edit'
   end
-  namespace :public do
-    get 'homes/top'
-    get 'homes/about'
-  end
-  namespace :admin do
-    get 'comments/index'
-    get 'comments/show'
-    get 'comments/edit'
-  end
-  namespace :admin do
-    get 'tags/index'
-    get 'tags/edit'
-  end
-  namespace :admin do
-    get 'end_users/index'
-    get 'end_users/show'
-    get 'end_users/edit'
-  end
-  namespace :admin do
-    get 'homes/top'
-  end
-  # 管理者
-  devise_for :admin, skip: [:registrations, :passwords] ,controllers: {
-  sessions: "admin/sessions"
-  }
-
-  # 会員
-  devise_for :end_users,skip: [:passwords], controllers: {
-  registrations: "public/registrations",
-  sessions: 'public/sessions'
-  }
 
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+end
 end
