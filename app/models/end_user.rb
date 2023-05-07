71dbfeacf0dcfb6a6e89799834712c200c94fc86
DWC-user07-pro01-4M <124852092+DWC-user07-pro01-4M@users.noreply.github.com>
2023-05-07 07:48:42 +0000

    Add 投稿機能実装のため会員と投稿のモデルに記述を追加

--- app/models/end_user.rb
+++ app/models/end_user.rb
@@ -5,4 +5,14 @@ class EndUser < ApplicationRecord
          :recoverable, :rememberable, :validatable
   # 追加・バリデーション
   validates :nickname, length: { minimum: 2, maximum: 10 }, presence: true
+  # 追加
+  has_one_attached :image
+  def get_image(width, height)
+    unless image.attached?
+      file_path = Rails.root.join("app/assets/images/no_image.png")
+      image.attach(io: File.open(file_path), filename: "no_image.png", content_type: "image/png")
+    end
+    image.variant(resize: "#{width}x#{height}^", gravity: "center", crop: "#{width}x#{height}+0+0").processed
+  end
+
 end
--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,17 @@
 class Post < ApplicationRecord
+
+  # 追加
+  has_one_attached :image
+  def get_image(width, height)
+    unless image.attached?
+      file_path = Rails.root.join("app/assets/images/no_image.png")
+      image.attach(io: File.open(file_path), filename: "no_image.png", content_type: "image/png")
+    end
+    image.variant(resize: "#{width}x#{height}^", gravity: "center", crop: "#{width}x#{height}+0+0").processed
+  end
+  # バリデーション
+  validates :facility_name, presence: true
+  validates :address, presence: true
+  validates :detailed_description, presence: true, length: {maximum:100}
+
 end
