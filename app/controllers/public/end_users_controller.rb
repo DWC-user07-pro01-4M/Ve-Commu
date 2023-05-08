b0f8e4039688fe3ffa7a069547e1b50c6c3b1a99
DWC-user07-pro01-4M <124852092+DWC-user07-pro01-4M@users.noreply.github.com>
2023-05-08 00:31:57 +0000

    Add 投稿ができるようにエンドユーザーのコントローラに記述を追加

--- app/controllers/public/end_users_controller.rb
+++ app/controllers/public/end_users_controller.rb
@@ -1,2 +1,26 @@
 class Public::EndUsersController < ApplicationController
+    before_action :authenticate_end_user!
+
+    def index
+    end
+
+    def show
+    end
+
+    def edit
+    end
+
+    def update
+    end
+
+    def unsubscribe
+    end
+
+    def withdrawal
+    end
+
+    private
+    def end_user_params
+    end
+
 end
