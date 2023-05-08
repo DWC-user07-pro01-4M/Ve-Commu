26f9c009008d42f412ce6bc34f5f4ba3327eeb02
DWC-user07-pro01-4M <124852092+DWC-user07-pro01-4M@users.noreply.github.com>
2023-05-08 00:44:20 +0000

    Add 投稿ができるようにエンドユーザーのコントローラに記述を追加

--- app/controllers/public/end_users_controller.rb
+++ app/controllers/public/end_users_controller.rb
@@ -2,25 +2,41 @@ class Public::EndUsersController < ApplicationController
     before_action :authenticate_end_user!
 
     def index
+        @end_users = EndUser.all
+        @post = Post.new
     end
 
     def show
+        @end_user = EndUser.find(params[:id])
+        @post = Post.new
     end
 
     def edit
+        @end_user = EndUser.find(current_end_user.id)
     end
 
     def update
+        @end_user = EndUser.find(current_end_user.id)
+        if @end_user.update(end_user_params)
+            redirect_to end_user_path(current_end_user.id), notice: "編集に成功しました。"
+        else
+            render :edit
+        end
     end
 
     def unsubscribe
     end
 
     def withdrawal
+        @end_user = EndUser.find(current_end_user.id)
+        @end_user.update(is_deleted: true)
+        reset_session
+        redirect_to root_path, notice: "退会処理が完了しました。ご利用ありがとうございました。"
     end
 
     private
     def end_user_params
+        params.require(:end_user).permit(:nickname)
     end
 
 end
