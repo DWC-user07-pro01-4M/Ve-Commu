08532034aa50cda5cd5c2ca4b25034ba547db813
DWC-user07-pro01-4M <124852092+DWC-user07-pro01-4M@users.noreply.github.com>
2023-05-08 13:58:45 +0000

    Add admin/end_userのコントローラに記述を追加

--- app/controllers/admin/end_users_controller.rb
+++ app/controllers/admin/end_users_controller.rb
@@ -1,10 +1,30 @@
 class Admin::EndUsersController < ApplicationController
-  def index
+  before_action :authenticate_admin!
+
+  def index #会員一覧
+    @end_users = EndUser.page(params[:page])
   end
 
-  def show
+  def show #会員詳細
+    @end_user = EndUser.find(params[:id])
   end
 
-  def edit
+  def edit #会員編集
+    @end_user = EndUser.find(params[:id])
   end
+
+  def update #会員情報更新
+    @end_user = EndUser.find(params[:id])
+    if @end_user.update(end_user_params)
+      redirect_to admin_end_user_path(@end_user.id), notice: "会員情報の更新に成功しました。"
+    else
+      render :edit
+    end
+  end
+
+  private
+  def end_user_params               #ニックネーム、メールアドレス、自己紹介文、退会ステータス
+    params.require(:end_user).permit(:nicknamem, :email, :introduction, :is_deleted)
+  end
+
 end
