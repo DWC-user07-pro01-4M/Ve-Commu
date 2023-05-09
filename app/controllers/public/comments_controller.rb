5c15e2f2076bc63293cc15a521f8472db3ba2436
DWC-user07-pro01-4M <124852092+DWC-user07-pro01-4M@users.noreply.github.com>
2023-05-09 07:02:31 +0000

    Add コメント機能実装途中

--- app/controllers/public/comments_controller.rb
+++ app/controllers/public/comments_controller.rb
@@ -1,2 +1,18 @@
 class Public::CommentsController < ApplicationController
+  before_action :authenticate_end_user!
+
+  def create
+    @comment = current_user.comments.new(comment_params)
+    if @comment.save
+      redirect_to public_post_path(@post)
+    else
+      render :show
+    end
+  end
+
+  private
+  def comment_params
+    params.require(:comment).permit(:comment, :post_id)
+  end
+
 end
--- app/controllers/public/posts_controller.rb
+++ app/controllers/public/posts_controller.rb
@@ -2,26 +2,32 @@ class Public::PostsController < ApplicationController
   before_action :authenticate_end_user!
 
   def create
-    @post = Post.new(post_params)
-    @post.end_user_id = current_end_user.id
+    # @post = Post.new(post_params)
+    # @post.end_user_id = current_end_user.id
+    @post = current_end_user.posts.new(post_params)
     if @post.save
       # @post.save_tags(params[:post][:tag])
       redirect_to public_post_path(@post), notice: "ありがとうございます。情報のシェアに成功しました。"
     else
-      @posts = Post.all
+      # @posts = Post.all
       render :index
     end
   end
 
   def index
-    @posts = Post.all
-    @post = Post.new
+    # @posts = Post.all
+    # @post = Post.new
+    @posts = current_end_user.posts.all
+    @post = current_end_user.posts.new
   end
 
   def show
+    # @post = Post.find(params[:id])
+    # @comments = @post.comments.order(created_at: :desc)
+    # @comment = Comment.new
     @post = Post.find(params[:id])
-    @comments = @post.comments.order(created_at: :desc)
-    @comment = Comment.new
+    @comments = @post.comments
+    @comment = current_end_user.comments.new
   end
 
   def edit
