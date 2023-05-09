dd6c65a8685b255cab87cf0c796f127f96a8e478
DWC-user07-pro01-4M <124852092+DWC-user07-pro01-4M@users.noreply.github.com>
2023-05-09 12:56:36 +0000

    Fix ログイン後の遷移先をマイページへ変更

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -8,6 +8,10 @@ class ApplicationController < ActionController::Base
     devise_parameter_sanitizer.permit(:sign_up, keys: [:nickname])
   end
 
+  def after_sign_in_path_for(resource)
+    end_user_path(resource)
+  end
+
   def after_sign_out_path_for(resource_or_scope)
     if resource_or_scope == :public
        root_path
