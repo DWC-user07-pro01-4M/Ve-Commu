aa1fe216349dcf60ec2eaa8841460c423346837b
DWC-user07-pro01-4M <124852092+DWC-user07-pro01-4M@users.noreply.github.com>
2023-05-10 06:20:49 +0000

    Add コメント機能を実装する

--- app/controllers/public/comments_controller.rb
+++ app/controllers/public/comments_controller.rb
@@ -2,22 +2,22 @@ class Public::CommentsController < ApplicationController
   before_action :authenticate_end_user!
 
   def create
-    @post = Post.find(params[post:id])
-    @comment = current_end_user.comments.new(comment_params)
-    @comment.post_id = post.id
+    @post = Post.find(params[:post_id])
+    @comment = @post.comments.build(comment_params)
+    @comment.end_user_id = current_end_user.id
     @comment.save
-    render :show
+    redirect_to request.referer
   end
 
   def destroy
     @comment = Comment.find(params[:id])
     @comment.destroy
-    render :show
+    redirect_to request.referer
   end
 
   private
   def comment_params
-    params.require(:comment).permit(:comment)
+    params.require(:comment).permit(:comment, :post_id, :user_id)
   end
 
 end
--- app/models/comment.rb
+++ app/models/comment.rb
@@ -2,5 +2,7 @@ class Comment < ApplicationRecord
 
   belongs_to :end_user
   belongs_to :post
+  # バリデーション
+  validates :comment,length:{ maximum: 200}, presence: true
 
 end
