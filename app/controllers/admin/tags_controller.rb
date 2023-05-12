832ccd7100cd34ff66a3bfa56de45a497ce4cb53
DWC-user07-pro01-4M <124852092+DWC-user07-pro01-4M@users.noreply.github.com>
2023-05-12 01:57:52 +0000

    Fix adminでタグの追加更新削除が行えるようにする

--- app/controllers/admin/tags_controller.rb
+++ app/controllers/admin/tags_controller.rb
@@ -8,31 +8,31 @@ class Admin::TagsController < ApplicationController
 
   def create #タグ作成
     @tag = Tag.new(tag_params)
-    if @tag.save!
-      redirect_to admin_tags_path, notice = "新しいタグを追加しました。"
+    if @tag.save
+      redirect_to admin_tags_path, notice: "新しいタグを追加しました。"
     else
       @tags = Tag.all
-      render :index, notice = "タグの追加に失敗しました。"
+      render :index, notice: "タグの追加に失敗しました。"
     end
   end
 
   def edit
-    @tag = Tag.find(params:id)
+    @tag = Tag.find(params[:id])
   end
 
   def update #タグ更新
     @tag = Tag.find(params[:id])
     if @tag.update(tag_params)
-      redirect_to admin_tags_path notice = "タグの更新に成功しました。"
+      redirect_to admin_tags_path, notice: "タグの更新に成功しました。"
     else
-      render :edit, notice = "タグの更新に失敗しました。"
+      render :edit, notice: "タグの更新に失敗しました。"
     end
   end
 
   def destroy #タグ削除
     tag = Tag.find(params[:id])
     tag.destroy
-    redirect_to admin_tags_path, notice = "タグを削除しました。"
+    redirect_to admin_tags_path, notice: "タグを削除しました。"
   end
 
   private
