18b21e51ff1b142fcc92a6a4565126fdf965ef67
DWC-user07-pro01-4M <124852092+DWC-user07-pro01-4M@users.noreply.github.com>
2023-05-12 01:21:47 +0000

    Add adminのtagコントローラに作成、更新、削除までができるように、フラッシュメッセージも含めて追記

--- app/controllers/admin/tags_controller.rb
+++ app/controllers/admin/tags_controller.rb
@@ -1,18 +1,40 @@
 class Admin::TagsController < ApplicationController
-  def index
+  before_action :authenticate_admin!
+
+  def index #タグ一覧
+    @tag = Tag.new
+    @tags = Tag.all
   end
 
-  def create
+  def create #タグ作成
+    @tag = Tag.new(tag_params)
+    if @tag.save!
+      redirect_to admin_tags_path, notice = "新しいタグを追加しました。"
+    else
+      @tags = Tag.all
+      render :index, notice = "タグの追加に失敗しました。"
+    end
   end
 
-  def update
+  def update #タグ更新
+    @tag = Tag.find(params[:id])
+    if @tag.update(tag_params)
+      redirect_to admin_tags_pathm notice = "タグの更新に成功しました。"
+    else
+      @tags = Tag.all
+      render :index, notice = "タグの更新に失敗しました。"
+    end
   end
 
-  def destroy
+  def destroy #タグ削除
+    tag = Tag.find(params[:id])
+    tag.destroy
+    redirect_to admin_tags_path, notice = "タグを削除しました。"
   end
 
   private
   def tag_params
+    params.require(:tag).permit(:tag_type, :tag_id)
   end
 
 end
