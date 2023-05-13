bdd34191cb445750f20dbd0ccd3acf438d7006cb
DWC-user07-pro01-4M <124852092+DWC-user07-pro01-4M@users.noreply.github.com>
2023-05-13 13:58:47 +0000

    Fix 新規投稿ページのレイアウトを修正、エラーメッセージも表示

--- app/models/tag.rb
+++ app/models/tag.rb
@@ -4,4 +4,7 @@ class Tag < ApplicationRecord
   has_many :association_post_and_tags, dependent: :destroy
   has_many :posts, through: :association_post_and_tags
 
+  # バリデーション
+  validates :tag_type, presence: true
+
 end
