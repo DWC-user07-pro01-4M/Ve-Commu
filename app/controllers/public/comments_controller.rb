eb8182bd1c45d6e9f8267ab2ae9667476b45e780
DWC-user07-pro01-4M <124852092+DWC-user07-pro01-4M@users.noreply.github.com>
2023-05-19 01:39:56 +0000

    Fix コメントコントローラの記述を修正

--- app/controllers/public/comments_controller.rb
+++ app/controllers/public/comments_controller.rb
@@ -3,34 +3,35 @@ class Public::CommentsController < ApplicationController
 
   def create
     @post = Post.find(params[:post_id])
-    # @comment = @post.comments.build(comment_params)
-    # @comment.end_user_id = current_end_user.id
-    @comment = current_end_user.comments.new(comment_params)
-    @comment.post_id = @post.id
+    @comment = @post.comments.build(comment_params)
+    @comment.end_user_id = current_end_user.id
     if @comment.save
-    # redirect_to request.referer
+      redirect_to request.referer
       # redirect_to post_path(@post), notice: "コメントを投稿しました。"
-      flash.now[:notice] = "コメントを投稿しました"
-      render :post_comments
+      # flash.now[:notice] = "コメントを投稿しました"
+      # render :post_comments
     else
       redirect_to request.referer
     end
   end
 
   def destroy
-    # @comment = Comment.find(params[:id])
-    # @comment.destroy
-    # redirect_to request.referer
-    Comment.find_by(id: params[:id], post_id: params[:post_id]).destroy
-    # redirect_to post_path(params[:post_id]), alert: "コメントを削除しました。"
-    flash.now[:alert] = "投稿を削除しました。"
-    @post = Post.find(params[:post_id])
-    render :post_comments
+    @comment = Comment.find(params[:id])
+    if @comment.destroy
+      redirect_to request.referer
+    # Comment.find_by(id: params[:id], post_id: params[:post_id]).destroy
+    # # redirect_to post_path(params[:post_id]), alert: "コメントを削除しました。"
+    # flash.now[:alert] = "投稿を削除しました。"
+    # @post = Post.find(params[:post_id])
+    # render :post_comments
+    else
+      redirect_to request.referer
+    end
   end
 
   private
   def comment_params
-    params.require(:comment).permit(:comment)
+    params.require(:comment).permit(:comment, :post_id, :end_user_id)
   end
 
 end
