d07a99bae0480844d7fd12c854b3f4f408788425
DWC-user07-pro01-4M <124852092+DWC-user07-pro01-4M@users.noreply.github.com>
2023-05-19 02:52:45 +0000

    Remove 不要なフォルダやファイル記述を削除

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,8 +2,6 @@ class ApplicationController < ActionController::Base
 
   # 追加
   before_action :configure_permitted_parameters, if: :devise_controller?
-  # Bootstrapに用意されているスタイルのフラッシュを定義
-  add_flash_types :success, :info, :warning, :danger
 
   private
 
