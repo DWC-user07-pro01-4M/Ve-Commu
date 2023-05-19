ac6a3a4f1e4dc8ac5856aeac509a53dc03530887
DWC-user07-pro01-4M <124852092+DWC-user07-pro01-4M@users.noreply.github.com>
2023-05-19 00:31:46 +0000

    Fix コメント非同期通信のためコントローラの記述を修正

--- app/controllers/public/comments_controller.rb
+++ app/controllers/public/comments_controller.rb
@@ -3,16 +3,24 @@ class Public::CommentsController < ApplicationController
 
   def create
     @post = Post.find(params[:post_id])
-    @comment = @post.comments.build(comment_params)
-    @comment.end_user_id = current_end_user.id
-    @comment.save
-    redirect_to request.referer
+    # @comment = @post.comments.build(comment_params)
+    # @comment.end_user_id = current_end_user.id
+    @comment = current_end_user.comments.new(comment_params)
+    @comment.post_id = @post.id
+    if @comment.save
+    # redirect_to request.referer
+      redirect_to post_path(@post), notice: "コメントを投稿しました"
+    else
+      render :show
+    end
   end
 
   def destroy
-    @comment = Comment.find(params[:id])
-    @comment.destroy
-    redirect_to request.referer
+    # @comment = Comment.find(params[:id])
+    # @comment.destroy
+    # redirect_to request.referer
+    Comment.find_by(id: params[:id], post_id: params[:post_id]).destroy
+    redirect_to post_path(params[:post_id]), alert: "コメントを削除しました"
   end
 
   private
