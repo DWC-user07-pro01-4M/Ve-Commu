12733f798a0c4e0241a9c1670ed852682a4ef23d
DWC-user07-pro01-4M <124852092+DWC-user07-pro01-4M@users.noreply.github.com>
2023-05-30 14:07:05 +0000

    Fix リダイレクト先を修正

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -8,7 +8,7 @@ class ContactsController < ApplicationController
     @contact = Contact.new(contact_params)
     if @contact.save
       ContacMailer.contact_mail(@contact, current_end_user).deliver
-      redirect_to root_path, notice: "お問い合わせ内容を送信しました。"
+      redirect_to thanks_contacts_path, notice: "お問い合わせ内容を送信しました。"
     else
       flash[:alert] = "お問い合わせの送信に失敗しました。"
       render :new
