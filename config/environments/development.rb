abc262eb8bc17f18b136c39586550659029e37a5
DWC-user07-pro01-4M <124852092+DWC-user07-pro01-4M@users.noreply.github.com>
2023-05-30 14:34:52 +0000

    Fix　記述を一部修正.envファイルに２点追記

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -82,7 +82,7 @@ Rails.application.configure do
     address:              'smtp.gmail.com',
     port:                  587,
     domain:               'gmail.com',
-    user_name:            ENV["ACTION_MAILER_END_USER"],
+    user_name:            ENV["ACTION_MAILER_USER"],
     password:             ENV["ACTION_MAILER_KEY"],
     authentication:       'plain',
     enable_starttls_auto:  true
