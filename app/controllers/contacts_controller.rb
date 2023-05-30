972160df7261cf933cb6f5b4cd3731d23861287c
DWC-user07-pro01-4M <124852092+DWC-user07-pro01-4M@users.noreply.github.com>
2023-05-30 13:52:21 +0000

    Add コントローラに定義を追記

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -1,2 +1,23 @@
 class ContactsController < ApplicationController
+
+  def new
+    @contact = Contact.new
+  end
+
+  def create
+    @contact = Contact.new(contact_params)
+    if @contact.save
+      ContacMailer.contact_mail(@contact, current_end_user).deliver
+      redirect_to root_path, notice: "お問い合わせ内容を送信しました。"
+    else
+      flash[:alert] = "お問い合わせの送信に失敗しました。"
+      render :new
+    end
+  end
+
+  private
+  def contact_params
+    params.require(:contact).permit(:name, :email, :content)
+  end
+
 end
