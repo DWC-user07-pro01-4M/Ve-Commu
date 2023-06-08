3973f1761c2306136a6a461deb7d92fa7ebef03f
DWC-user07-pro01-4M <124852092+DWC-user07-pro01-4M@users.noreply.github.com>
2023-06-08 09:52:43 +0000

    Fix 不要な記述を削除

--- lib/batch/data_reset.rb
+++ lib/batch/data_reset.rb
@@ -3,7 +3,5 @@ class Batch::DataReset
   def self.data_reset
     end_user = EndUser.find_by(email: "guest@example.com")
     end_user.posts.destroy_all
-    end_user.comments.destroy_all
-    end_user.bookmarks.destroy_all
   end
 end
