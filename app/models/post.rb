bb56a382ce54ee9b6dee189fc87816de7808a3d1
DWC-user07-pro01-4M <124852092+DWC-user07-pro01-4M@users.noreply.github.com>
2023-06-13 07:45:26 +0000

    Add 投稿モデルに通知の定義を追記69-80行目

--- app/models/post.rb
+++ app/models/post.rb
@@ -49,13 +49,34 @@ class Post < ApplicationRecord
       notification = current_user.active_notifications.new(
         post_id: id,
         visited_id: user_id,
-        action: 'like'
+        action: "like"
       )
       if notification.visitor_id == notification.visited_id
         notification.checked = true
       end
       notification.save if notification.valid?
     end
+  end
+
+  def create_notification_comment!(current_end_user, comment_id)
+    temp_ids = Comment.select(:end_user_id).where(post_id: id).where.not(end_user_id: current_end_user.id).distinct
+    temp_ids.each do |temp_id|
+      save_notification_comment!(current_end_user, comment_id, temp_id["enduser_id"])
+    end
+    save_notification_comment!(current_end_user, comment_id, end_user_id) if temp_ids.blank?
+  end
+
+  def save_notification_comment!(current_end_user, comment_id, visited_id)
+    notification = current_user.active_notifications.new(
+      post_id: id,
+      comment_id: comment_id,
+      visited_id: visited_id,
+      action: "comment"
+    )
+    if notification.visitor_id == notification.visited_id
+      notification.checked = true
+    end
+    notification.save if notification.valid?
   end
 
   private
