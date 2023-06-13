5a74f56cc6d4b3f28c352cc813c76c1f02a09f42
DWC-user07-pro01-4M <124852092+DWC-user07-pro01-4M@users.noreply.github.com>
2023-06-13 02:35:42 +0000

    Fix いいね機能非同期通信に修正中

--- app/controllers/public/likes_controller.rb
+++ app/controllers/public/likes_controller.rb
@@ -4,14 +4,12 @@ class Public::LikesController < ApplicationController
     post =  Post.find(params[:post_id])
     like = current_end_user.likes.new(post_id: post.id)
     like.save
-    redirect_to posts_path
   end
 
   def destroy
     post =  Post.find(params[:post_id])
     like = current_end_user.likes.find_by(post_id: post.id)
     like.destroy
-    redirect_to books_path
   end
 
 end
