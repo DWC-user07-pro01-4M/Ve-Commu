b0d66a722bbb6a84a67d2b2ca730ede3de387529
DWC-user07-pro01-4M <124852092+DWC-user07-pro01-4M@users.noreply.github.com>
2023-06-28 00:24:12 +0000

    Fix バッチ処理が働いていないかったので修正

--- lib/batch/data_reset.rb
+++ lib/batch/data_reset.rb
@@ -1,11 +1,9 @@
 class Batch::DataReset
-    # guestuserの投稿を削除
   def self.data_reset
     end_user = EndUser.find_by(email: "guest@example.com")
     end_user.posts.destroy_all
     end_user.comments.destroy_all
     end_user.bookmarks.destroy_all
     end_user.likes.destroy_all
-    end_user.notifications.destroy_all
   end
 end
